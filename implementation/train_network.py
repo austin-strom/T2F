28426428df10b0339ed8f345da941c03364b15ed
Austin Strom <austinjstrom@gmail.com>
2021-04-12 19:47:53 -0500

    Fixing got Tensor instead of numpy.ndarray error and issue where inputs and outputs were not on same device

--- implementation/train_network.py
+++ implementation/train_network.py
@@ -136,8 +136,10 @@ def train_networks(encoder, ca, c_pro_gan, dataset, epochs,
     # create fixed_input for debugging
     temp_data = dl.get_data_loader(dataset, batch_sizes[start_depth], num_workers=3)
     fixed_captions, fixed_real_images = iter(temp_data).next()
+    fixed_captions.to(device)
     fixed_embeddings = encoder(fixed_captions)
-    fixed_embeddings = th.from_numpy(fixed_embeddings).to(device)
+    
+    fixed_embeddings = (fixed_embeddings).to(device)
 
     fixed_c_not_hats, _, _ = ca(fixed_embeddings)
 
@@ -192,7 +194,7 @@ def train_networks(encoder, ca, c_pro_gan, dataset, epochs,
                 images = images.to(device)
 
                 # perform text_work:
-                embeddings = th.from_numpy(encoder(captions)).to(device)
+                embeddings = (encoder(captions).to(device)
                 if encoder_optim is None:
                     # detach the LSTM from backpropagation
                     embeddings = embeddings.detach()
